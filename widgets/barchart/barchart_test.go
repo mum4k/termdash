774ad2479a25592d9242ddfbb1c5aed0b877b6f6
Jakub Sobon <jakub.sobon@elohim.sk>
2018-06-18 22:36:20 +0100

    More test cases.

--- widgets/barchart/barchart_test.go
+++ widgets/barchart/barchart_test.go
@@ -7,6 +7,9 @@ import (
 	"github.com/kylelemons/godebug/pretty"
 	"github.com/mum4k/termdash/canvas"
 	"github.com/mum4k/termdash/canvas/testcanvas"
+	"github.com/mum4k/termdash/cell"
+	"github.com/mum4k/termdash/draw"
+	"github.com/mum4k/termdash/draw/testdraw"
 	"github.com/mum4k/termdash/terminal/faketerm"
 	"github.com/mum4k/termdash/widgetapi"
 )
@@ -22,22 +25,399 @@ func TestGauge(t *testing.T) {
 		wantDrawErr   bool
 	}{
 		{
+			desc: "draws empty for no values",
+			bc: New(
+				Char('o'),
+			),
+			update: func(bc *BarChart) error {
+				return nil
+			},
+			canvas: image.Rect(0, 0, 3, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				return faketerm.MustNew(size)
+			},
+		},
+		{
 			desc: "displays bars",
+			bc: New(
+				Char('o'),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{0, 2, 5, 10}, 10)
+			},
+			canvas: image.Rect(0, 0, 7, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(4, 5, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(6, 0, 7, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "displays bars with labels",
+			bc: New(
+				Char('o'),
+				Labels([]string{
+					"1",
+					"2",
+					"3",
+				}),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2, 5, 10}, 10)
+			},
+			canvas: image.Rect(0, 0, 7, 11),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(4, 5, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(6, 0, 7, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+
+				// Labels.
+				testdraw.MustText(c, "1 2 3", image.Point{0, 10}, draw.TextCellOpts(
+					cell.FgColor(DefaultLabelColor),
+				))
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "trims too long labels",
+			bc: New(
+				Char('o'),
+				Labels([]string{
+					"1",
+					"22",
+					"3",
+				}),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2, 5, 10}, 10)
+			},
+			canvas: image.Rect(0, 0, 7, 11),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(4, 5, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(6, 0, 7, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+
+				// Labels.
+				testdraw.MustText(c, "1 … 3", image.Point{0, 10}, draw.TextCellOpts(
+					cell.FgColor(DefaultLabelColor),
+				))
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "displays bars with labels and values",
+			bc: New(
+				Char('o'),
+				Labels([]string{
+					"1",
+					"2",
+					"3",
+				}),
+				ShowValues(),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2, 5, 10}, 10)
+			},
+			canvas: image.Rect(0, 0, 7, 11),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(4, 5, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(6, 0, 7, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				// Labels.
+				testdraw.MustText(c, "1 2 3", image.Point{0, 10}, draw.TextCellOpts(
+					cell.FgColor(DefaultLabelColor),
+				))
+				// Values.
+				testdraw.MustText(c, "1 2 5 …", image.Point{0, 9}, draw.TextCellOpts(
+					cell.FgColor(DefaultValueColor),
+					cell.BgColor(DefaultBarColor),
+				))
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "bars take as much width as available",
 			bc: New(
 				Char('o'),
 			),
 			update: func(bc *BarChart) error {
 				return bc.Values([]int{1, 2}, 10)
 			},
-			canvas: image.Rect(0, 0, 3, 10),
+			canvas: image.Rect(0, 0, 5, 10),
 			want: func(size image.Point) *faketerm.Terminal {
 				ft := faketerm.MustNew(size)
 				c := testcanvas.MustNew(ft.Area())
 
-				//testdraw.MustRectangle(c, image.Rect(0, 0, 3, 3),
-				//	draw.RectChar('o'),
-				//	draw.RectCellOpts(cell.BgColor(cell.ColorRed)),
-				//)
+				testdraw.MustRectangle(c, image.Rect(0, 9, 2, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(3, 8, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "respects set bar width",
+			bc: New(
+				Char('o'),
+				BarWidth(1),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2}, 10)
+			},
+			canvas: image.Rect(0, 0, 5, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "options can be set on a call to Values",
+			bc:   New(),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2}, 10, Char('o'), BarWidth(1))
+			},
+			canvas: image.Rect(0, 0, 5, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "respects set bar gap",
+			bc: New(
+				Char('o'),
+				BarGap(2),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2}, 10)
+			},
+			canvas: image.Rect(0, 0, 5, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(4, 8, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "respects both width and gap",
+			bc: New(
+				Char('o'),
+				BarGap(2),
+				BarWidth(2),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{5, 3}, 10)
+			},
+			canvas: image.Rect(0, 0, 6, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 5, 2, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustRectangle(c, image.Rect(4, 7, 6, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "respects bar and label colors",
+			bc: New(
+				Char('o'),
+				BarColors([]cell.Color{
+					cell.ColorBlue,
+					cell.ColorYellow,
+				}),
+				LabelColors([]cell.Color{
+					cell.ColorCyan,
+					cell.ColorMagenta,
+				}),
+				Labels([]string{
+					"1",
+					"2",
+				}),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{1, 2, 3}, 10)
+			},
+			canvas: image.Rect(0, 0, 5, 11),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustRectangle(c, image.Rect(0, 9, 1, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(cell.ColorBlue)),
+				)
+				testdraw.MustText(c, "1", image.Point{0, 10}, draw.TextCellOpts(
+					cell.FgColor(cell.ColorCyan),
+				))
+
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(cell.ColorYellow)),
+				)
+				testdraw.MustText(c, "2", image.Point{2, 10}, draw.TextCellOpts(
+					cell.FgColor(cell.ColorMagenta),
+				))
+
+				testdraw.MustRectangle(c, image.Rect(4, 7, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustText(c, "3", image.Point{4, 10}, draw.TextCellOpts(
+					cell.FgColor(DefaultLabelColor),
+				))
+				testcanvas.MustApply(c, ft)
+				return ft
+			},
+		},
+		{
+			desc: "respects value colors",
+			bc: New(
+				Char('o'),
+				ValueColors([]cell.Color{
+					cell.ColorBlue,
+					cell.ColorBlack,
+				}),
+				ShowValues(),
+			),
+			update: func(bc *BarChart) error {
+				return bc.Values([]int{0, 2, 3}, 10)
+			},
+			canvas: image.Rect(0, 0, 5, 10),
+			want: func(size image.Point) *faketerm.Terminal {
+				ft := faketerm.MustNew(size)
+				c := testcanvas.MustNew(ft.Area())
+
+				testdraw.MustText(c, "0", image.Point{0, 9}, draw.TextCellOpts(
+					cell.FgColor(cell.ColorBlue),
+				))
+
+				testdraw.MustRectangle(c, image.Rect(2, 8, 3, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustText(c, "2", image.Point{2, 9}, draw.TextCellOpts(
+					cell.FgColor(cell.ColorBlack),
+					cell.BgColor(DefaultBarColor),
+				))
+
+				testdraw.MustRectangle(c, image.Rect(4, 7, 5, 10),
+					draw.RectChar('o'),
+					draw.RectCellOpts(cell.BgColor(DefaultBarColor)),
+				)
+				testdraw.MustText(c, "3", image.Point{4, 9}, draw.TextCellOpts(
+					cell.FgColor(DefaultLabelColor),
+					cell.BgColor(DefaultBarColor),
+				))
 				testcanvas.MustApply(c, ft)
 				return ft
 			},

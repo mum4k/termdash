38aae8e5565186a027096b0bd0622686a6c304e2
Jakub Sobon <jakub.sobon@elohim.sk>
2019-02-04 21:41:04 -0500

    Switching text to attrrange.

--- widgets/text/write_options.go
+++ widgets/text/write_options.go
@@ -17,8 +17,6 @@ package text
 // write_options.go contains options used when writing content to the Text widget.
 
 import (
-	"sort"
-
 	"github.com/mum4k/termdash/cell"
 )
 
@@ -57,61 +55,4 @@ func WriteCellOpts(opts ...cell.Option) WriteOption {
 	return writeOption(func(wOpts *writeOptions) {
 		wOpts.cellOpts = cell.NewOptions(opts...)
 	})
-}
-
-// optsRange are write options that apply to a range of bytes in the text.
-type optsRange struct {
-	// low is the first byte where these options apply.
-	low int
-
-	// high is the end of the range. The opts apply to all bytes in range low
-	// <= b < high.
-	high int
-
-	// opts are the options provided at a call to Write().
-	opts *writeOptions
-}
-
-// newOptsRange returns a new optsRange.
-func newOptsRange(low, high int, opts *writeOptions) *optsRange {
-	return &optsRange{
-		low:  low,
-		high: high,
-		opts: opts,
-	}
-}
-
-// givenWOpts stores the write options provided on all the calls to Write().
-// The options are keyed by their low indices.
-type givenWOpts map[int]*optsRange
-
-// newGivenWOpts returns a new givenWOpts instance.
-func newGivenWOpts() givenWOpts {
-	return givenWOpts{}
-}
-
-// forPosition returns write options that apply to character at the specified
-// byte position.
-func (g givenWOpts) forPosition(pos int) *optsRange {
-	if or, ok := g[pos]; ok {
-		return or
-	}
-
-	var keys []int
-	for k := range g {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-
-	res := newOptsRange(0, 0, newWriteOptions())
-	for _, k := range keys {
-		or := g[k]
-		if or.low > pos {
-			break
-		}
-		if or.high > pos {
-			res = or
-		}
-	}
-	return res
 }

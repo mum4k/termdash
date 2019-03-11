a170c5f99ddcac9bd09f37bc1572890fc47a4e15
Jakub Sobon <jakub.sobon@elohim.sk>
2019-03-10 23:54:42 -0400

    Fixing lint issues.

--- container/grid/grid.go
+++ container/grid/grid.go
@@ -1,17 +1,3 @@
-// Copyright 2019 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 // Package grid helps to build grid layouts.
 package grid
 
@@ -119,9 +105,8 @@ func build(elems []Element, parentHeightPerc, parentWidthPerc int) []container.O
 					container.SplitPercent(perc),
 				),
 			}
-		} else {
-			return build(e.subElem, 100, parentWidthPerc)
 		}
+		return build(e.subElem, 100, parentWidthPerc)
 
 	case *col:
 		if len(elems) > 0 {
@@ -134,9 +119,8 @@ func build(elems []Element, parentHeightPerc, parentWidthPerc int) []container.O
 					container.SplitPercent(perc),
 				),
 			}
-		} else {
-			return build(e.subElem, parentHeightPerc, 100)
 		}
+		return build(e.subElem, parentHeightPerc, 100)
 
 	case *widget:
 		opts := e.cOpts
